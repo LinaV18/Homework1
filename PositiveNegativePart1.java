cee538e808b45630baae42d66ab09d7c26af012a
Lina Valand <linavaland@hotmail.co.uk>
2022-07-28 20:49:40 +0100

    homework

---
+++ PositiveNegativePart1.java
@@ -0,0 +1,13 @@
+package Homework;
+
+public class PositiveNegativePart1 {
+    public static void main(String[] args) {
+
+        int a = 5;
+        if (a > 0) {
+            System.out.println(a + " is positive");
+        } else if (a < 0) {
+            System.out.println(a + " is negative");
+        }
+    }
+}
---
+++ VotingHomework.java
@@ -0,0 +1,16 @@
+package Homework;
+
+public class VotingHomework {
+    public static void main(String[] args){
+        int a = 25;
+        int b = 18;
+        if (a > b){
+            System.out.println("candidate can vote");
+    }     else
+            System.out.println("candidate cannot vote");
+
+        }
+
+    }
+
+
